c47cf1a4e6707b2d55e82ba507c772ee95304941
Takumikimura141899 <kimura141899@gmail.com>
2021-12-19 20:44:06 +0900

    [add] formのひな型

---
+++ @types/global.d.ts
@@ -0,0 +1,10 @@
+type FormContents = {
+  image?: any;
+  title: string;
+  genre: BookGenre;
+  author: string;
+  page: number;
+  status: '読了' | '読中' | '未読';
+};
+
+type BookGenre = 'マンガ' | '雑誌' | 'ビジネス' | '文学' | 'IT' | '趣味';
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,34 +1,58 @@
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
-type Inputs = {
-  title: string;
-  genre: string;
-};
-
 export const Form = () => {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  } = useForm<FormContents>();
+  const onSubmit: SubmitHandler<FormContents> = (data) => console.log(data);
 
-  console.log(watch('title')); // watch input value by passing the name of it
+  console.log(watch('title'));
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form className='flex flex-col w-4/5' onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input className='border-8' placeholder='title' {...register('title')} />
-
-      {/* include validation with required or other standard HTML validation rules */}
-      <input className='border-8' {...register('genre', { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.genre && <span>This field is required</span>}
-
+      <input className='border-8' type='file' {...register('image')} />
+      <input
+        className='border-8'
+        placeholder='title'
+        {...register('title', { required: true })}
+      />
+      {errors.title && <span>This field is required</span>}
+      <select className='border-8' {...register('genre', { required: true })}>
+        <option value='マンガ'>マンガ</option>
+        <option value='雑誌'>雑誌</option>
+        <option value='ビジネス'>ビジネス</option>
+        <option value='文学'>文学</option>
+        <option value='IT'>IT</option>
+        <option value='趣味'>趣味</option>
+      </select>
+      <input
+        className='border-8'
+        placeholder='author'
+        {...register('author', { required: true })}
+      />
+      {errors.author && <span>This field is required</span>}
+      <input
+        type='number'
+        className='border-8'
+        placeholder='page'
+        {...register('page', { required: true })}
+      />
+      {errors.page && <span>This field is required</span>}
+      <select
+        className='border-8'
+        defaultValue='未読'
+        {...register('status', { required: true })}
+      >
+        <option value='未読'>未読</option>
+        <option value='読中'>読中</option>
+        <option value='読了'>読了</option>
+      </select>
       <input className='border-8' type='submit' />
     </form>
   );
 };
+type BookGenre = 'IT' | '雑誌' | 'ビジネス' | '文学' | 'IT' | '趣味';
