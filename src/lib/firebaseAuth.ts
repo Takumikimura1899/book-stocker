63d254a09d725f18a326c9d4446ef29d0baddd36
Takumikimura141899 <kimura141899@gmail.com>
2022-01-16 21:14:26 +0900

    [add] ログイン時に初めてのユーザーだったらcollectionに追加する処理を追加

--- src/lib/firebaseAuth.ts
+++ src/lib/firebaseAuth.ts
@@ -1,21 +1,27 @@
+import { addDoc, collection } from 'firebase/firestore';
 import {
   getAuth,
-  onAuthStateChanged,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
+  getAdditionalUserInfo,
 } from 'firebase/auth';
-import { firebaseApp } from './firebase';
+import { db, firebaseApp } from './firebase';
 
 export const auth = getAuth(firebaseApp);
 
 export const provider = new GoogleAuthProvider();
 
-export const signInWithGoogle = () => {
+export const signInWithGoogle = async () => {
   signInWithPopup(auth, provider)
     .then((result) => {
       console.log('Googleアカウントでログインしました。');
       console.log(result);
+      const isNewUser = getAdditionalUserInfo(result)?.isNewUser;
+
+      if (isNewUser) {
+        addDoc(collection(db, 'user'), { id: result.user.uid });
+      }
     })
     .catch((error) => {
       console.error(error);
