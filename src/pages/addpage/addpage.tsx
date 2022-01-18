56867a444015f19ab40cba4a53170eed1750ceab
Takumikimura141899 <kimura141899@gmail.com>
2022-01-19 06:27:49 +0900

    addpageの余分な部分を削除

--- src/pages/addpage/addpage.tsx
+++ src/pages/addpage/addpage.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import { useState } from 'react';
 import { Layout } from '~/src/components/layout/Layout';
 import { Form } from '../../components/Form';
@@ -16,17 +14,6 @@ const AddPage: NextPage = () => {
       <div className=''>
         <Layout>
           <Form />
-          <button
-            onClick={() => {
-              const info = localStorage.getItem('info');
-              console.log(info);
-              console.log(image?.preview);
-              setShowImage(!showImage);
-            }}
-          >
-            ゲット
-          </button>
-          <Link href='/contents/contents'>firebaseContents</Link>
           {showImage && (
             <Image src={image!.preview} alt='image' width={100} height={100} />
           )}
